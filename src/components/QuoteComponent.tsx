05814073748b6a17c03bb1656cf71c7ee0c532a7
hktari <bkamnik1995@gmail.com>
2022-07-13 21:33:27 +0200

    move images to public folder and style text for qutoes card

--- src/components/QuoteComponent.tsx
+++ src/components/QuoteComponent.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { Quote, User } from '../services/interface'
 
-
-const QuoteComponent = ({id, text, user, voteCount, voteState} : Quote) => {
-
+const QuoteComponent = ({ id, text, user, voteCount, voteState }: Quote) => {
+    console.log(user);
+    console.log(user.profileImg.thumbnailUrl);
     return (
-        <div>
-            <blockquote>
-                {text}
-                <cite>{user.username}</cite>
-            </blockquote>
+        <div className='quote-card'>
+            <div className="votes">
+
+            </div>
+            <div className="details">
+                <p>{text}</p>
+                <div className="user-profile">
+                    <img src={user.profileImg.thumbnailUrl} alt="" />
+                    <span>{user.username}</span>
+                </div>
+            </div>
         </div>
     )
 }
