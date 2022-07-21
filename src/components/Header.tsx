738d51dbe906ffdabb8d6c0d030b2eee6043196d
hktari <bkamnik1995@gmail.com>
2022-07-21 14:27:58 +0200

    style side menu mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { userInfo } from 'os'
 import React from 'react'
 import { User } from '../services/interface'
 
@@ -7,39 +8,59 @@ type HeaderProps = {
 
 const Header = (props: HeaderProps) => {
     return (
-        <header>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Quotastic</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+        <>
+            <header>
+                <nav>
+                    <div>
+                        <a href="#" className='title-brand' ><h3>Quotastic<i className="bi bi-quote"></i></h3>
+                        </a>
+                        <button type="button"
+                            data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <i>OPEN/CLOSE</i>
+                        </button>
+                    </div>
+                </nav>
+            </header>
+            <div className="side-menu">
+                <div className="header">
+                    <button type="button"
+                        className='btn-toggle'
+                        data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i className='bi bi-x'></i>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            {props.user ? (
-                                <>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#">Settings</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#">Logout</a>
-                                    </li>
-                                </>
-                            ) : (
-                                <>
-                                    <button className="btn btn-positive">
-                                        Sign up
-                                    </button>
-                                    <button className="btn btn-outlined">Login</button>
-                                </>)}
-                        </ul>
+                    <div className="user-profile">
+                        <img src={props.user?.profileImg.thumbnailUrl} alt="" />
+                        <h5>{props.user?.username}</h5>
                     </div>
                 </div>
-            </nav>
-        </header>
+                <ul className='nav-list'>
+                    <li className='nav-item'>
+                        <a aria-current="page" href="#">Home</a>
+                        <i className="bi bi-chevron-right"></i>
+                    </li>
+                    {props.user ? (
+                        <>
+                            <li className='nav-item'>
+                                <a href="#">Settings</a>
+                                <i className="bi bi-chevron-right"></i>
+                            </li>
+                            <li className='nav-item'>
+                                <a href="#">Logout</a>
+                                <i className="bi bi-chevron-right"></i>
+                            </li>
+                        </>
+                    ) : (
+                        <>
+                            <button className="btn btn-positive">
+                                Sign up
+                            </button>
+                            <button className="btn btn-outlined">Login</button>
+                        </>)}
+                </ul>
+            </div>
+        </>
     )
 }
 
