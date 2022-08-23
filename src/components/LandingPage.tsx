f456a33820155340188be97326e00d67211d6a70
hktari <bkamnik1995@gmail.com>
2022-08-23 09:01:40 +0200

    layout landingpage for desktop

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import authApi from '../services/authApi'
-import { Quote, User } from '../services/interface'
+import { Quote, QuotesList, User } from '../services/interface'
 import quotesApi from '../services/quotesApi'
+import { EmptyQuotesList } from '../util/util'
 import { AuthContextType, useAuth } from './AuthProvider'
 import QuoteComponent from './QuoteComponent'
+import QuotesListComponent from './QuotesListComponent'
 import { useQuotes } from './QuotesProvider'
 
 
@@ -75,8 +77,19 @@ const LandingPage = () => {
         )
     }
 
+    async function loadMostUpvotedQuotes(startIdx: number, pageSize: number): Promise<QuotesList> {
+        try {
+            return await quotesApi.getMostUpvotedQuotes(startIdx, pageSize)
+        } catch (error) {
+            console.error(error)
+            window.alert('Error occured loading quotes')
+            return EmptyQuotesList()
+        }
+    }
+
+
     return (
-        <div className="container">
+        <div id='landing-page' className="container">
             <section id="quote-of-the-day">
                 <h4>Quote of the day</h4>
                 <p className="text-body text-center">
@@ -84,20 +97,34 @@ const LandingPage = () => {
                 </p>
                 <QuoteOfTheDay />
             </section>
-            <section id="most-liked-quotes">
-                <h3 className='text-center'>Most liked quotes</h3>
-                <p className="text-body text-center">
-                    Most liked quotes on the platform.  Sign up or login to like the quotes  and keep them saved in your profile
-                </p>
-                <MostLikedQuotes />
-            </section>
-            <section id="most-recent-quotes">
-                <h5>Most recent quotes</h5>
-                <p className="text-body">
-                    Recent quotes updates as soon user adds new quote. Go ahed show them that you seen the new quote and like the ones you like.
-                </p>
-                <MostRecentQuotes />
-            </section>
+
+
+            <div className="d-none d-md-block">
+                <section id="most-upvoted-quotes">
+                    <h4><em>Most upvoted quotes</em></h4>
+                    <p className="text-body text-center">Most upvoted quotes on the platform.
+                        Give a like to the ones you like to<br />
+                        keep them saved in your profile.</p>
+
+                    <QuotesListComponent loadMoreItems={loadMostUpvotedQuotes} />
+                </section>
+            </div>
+            <div className="d-md-none">
+                <section id="most-liked-quotes">
+                    <h5 className='text-center text-color-primary'>Most liked quotes</h5>
+                    <p className="text-body text-center">
+                        Most liked quotes on the platform.  Sign up or login to like the quotes  and keep them saved in your profile
+                    </p>
+                    <MostLikedQuotes />
+                </section>
+                <section id="most-recent-quotes">
+                    <h5 className='text-color-primary'>Most recent quotes</h5>
+                    <p className="text-body">
+                        Recent quotes updates as soon user adds new quote. Go ahed show them that you seen the new quote and like the ones you like.
+                    </p>
+                    <MostRecentQuotes />
+                </section>
+            </div>
             <div className="white-space"></div>
         </div>
     )
